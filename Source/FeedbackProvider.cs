754734bcd03a91c5a4ccd06c1ef46c5a1cf8bf5d
Justin Grote <JustinGrote@users.noreply.github.com>
2023-11-19 22:20:59 -0800

    ♻️ Revert Abstractions thanks to new E2E tests

--- Source/FeedbackProvider.cs
+++ Source/FeedbackProvider.cs
@@ -13,34 +13,6 @@ using static MicrosoftGraphAdvancedQueryFeedbackProvider.Strings;
 namespace MicrosoftGraphAdvancedQueryFeedbackProvider;
 
 /// <summary>
-/// This adapter is needed because FeedbackContext is <see langword="sealed"/> and not constructable for testing.
-/// </summary>
-interface FeedbackContextAdapter
-{
-  public string CommandLine { get; }
-  public Ast CommandLineAst { get; }
-  public IReadOnlyList<Token> CommandLineTokens { get; }
-  public string CurrentLocation { get; }
-  // ErrorRecord Invocation is not easily constructed so we abstract these to make it more testable.
-  public string? ErrorCommand { get; }
-  public string? ErrorMessage { get; }
-  public string? ErrorId { get; }
-  public object? ErrorTarget { get; }
-}
-
-class PSFeedbackContextAdapter(FeedbackContext context) : FeedbackContextAdapter
-{
-  public Ast CommandLineAst => context.CommandLineAst;
-  public string CommandLine => context.CommandLine;
-  public IReadOnlyList<Token> CommandLineTokens => context.CommandLineTokens;
-  public string CurrentLocation => context.CurrentLocation.ToString();
-  public string? ErrorCommand => context.LastError?.InvocationInfo.Statement;
-  public string? ErrorMessage => context.LastError?.ErrorDetails.Message;
-  public string? ErrorId => context.LastError?.FullyQualifiedErrorId;
-  public object? ErrorTarget => context.LastError?.TargetObject;
-}
-
-/// <summary>
 /// A feedback provider that looks for Microsoft Graph commands that require additional Advanced Query parameters and warns you of the same
 /// </summary>
 public class AqFeedbackProvider : IFeedbackProvider
@@ -56,19 +28,11 @@ public class AqFeedbackProvider : IFeedbackProvider
   FeedbackTrigger IFeedbackProvider.Trigger { get; } = FeedbackTrigger.All;
 
   public FeedbackItem? GetFeedback(FeedbackContext context, CancellationToken token)
-    => GetFeedbackImpl(new PSFeedbackContextAdapter(context));
-  #endregion
-
-  /// <summary>
-  /// This is the actual implementation of the feedback provider. It is separated for testing because the public interface has a <see langword="sealed"/> parameter that cannot be construted.
-  /// </summary>
-  ///
-  internal FeedbackItem? GetFeedbackImpl(FeedbackContextAdapter context)
   {
     HashSet<string> graphCommands = new HashSet<string>();
-    if (context.ErrorId is not null)
+    if (context.LastError is not null)
     {
-      var result = InspectGraphError(context.ErrorId, context.ErrorMessage, context.ErrorTarget, context.ErrorCommand);
+      var result = InspectGraphError(context.LastError);
       if (result is not null)
         graphCommands.Add(result);
     }
@@ -86,63 +50,66 @@ public class AqFeedbackProvider : IFeedbackProvider
       graphCommands.Select(command => command.ToString())
     );
   }
+  #endregion IFeedbackProvider
 
   /// <summary>
   /// Inspects the last error to see if it is a Graph error that needs Advanced Query
   /// </summary>
-  string? InspectGraphError(string fullyQualifiedErrorId, string? errorMessage, object? targetObject, string? statement)
+  string? InspectGraphError(ErrorRecord error)
   {
-    var errorCode = fullyQualifiedErrorId.Split(',')[0];
+    var errorMessage = error.Exception.Message;
+    var errorCommand = error.InvocationInfo.Statement;
+    var errorCode = error.FullyQualifiedErrorId.Split(',')[0];
 
     if (errorCode == "Request_BadRequest")
     {
       // TargetObject is an anonyous type so we have to use reflection to get this value
-      string? filter = targetObject?.GetType().GetProperty("Filter")?.GetValue
-      (targetObject, null) as string;
+      string? filter = error.TargetObject?.GetType().GetProperty("Filter")?.GetValue
+      (error.TargetObject, null) as string;
       if (filter is not null)
       {
         // Use of $count in a filter expression
         if (filter.Contains("/$count"))
         {
-          return statement;
+          return errorCommand;
         }
       }
     }
 
     if (errorCode == "Request_UnsupportedQuery")
     {
-      if (errorMessage is not null)
+      if (error.Exception.Message is not null)
       {
         // Use of $search
         if (errorMessage.Contains(SearchUnsupportedError))
-          return statement;
+          return errorCommand;
 
         // Use of $filter in endsWith
         // Use of $filter with not operator
         // Use of $filter with not and StartWith
         if (errorMessage.Contains(ConsistencyHeaderMissingError))
-          return statement;
+          return errorCommand;
 
         if (errorMessage.Contains(SortingNotSupportedError))
         {
           // Use of $filter and $orderby in the same query
           // TODO: This may match incorrectly in a complicated script, we should verify the commands we search are graph ones only.
-          var parameters = ScriptBlock.Create(statement).Ast.FindAll<CommandParameterAst>().Select(parameter => parameter.ParameterName).ToList();
+          var parameters = ScriptBlock.Create(errorCommand).Ast.FindAll<CommandParameterAst>().Select(parameter => parameter.ParameterName).ToList();
 
           if (parameters.Contains("OrderBy", StringComparer.OrdinalIgnoreCase))
-            return statement;
+            return errorCommand;
 
           return null;
         }
 
         // Use of filter with ne operator
         if (errorMessage.Contains(NotEqualsMatch))
-          return statement;
+          return errorCommand;
 
         // Various property errors
         // TODO: Check the individual property name support
         if (errorMessage.Contains(UnsupportedOrInvalidError))
-          return statement;
+          return errorCommand;
       }
     }
 
